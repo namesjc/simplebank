c9a56c9a9fcda80caa80f5939c4970e71fc2b2ba
namesjc <adiachan@outlook.com>
2022-04-12 15:49:26 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"simplebank/api"
+	db "simplebank/db/sqlc"
+	"simplebank/util"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
+}
