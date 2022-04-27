655ec9ed34ccce00f978eb5fed14bc3bb5981ab7
namesjc <adiachan@outlook.com>
2022-04-27 14:52:20 +0800

    docker

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ import (
 
 func main() {
 	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
 	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
